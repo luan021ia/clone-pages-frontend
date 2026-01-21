d0639797dc6a53893d48788437420eec135db319
Luan <luan@fabricadelowticket.com.br>
2026-01-21 11:49:06 -0300

    fix: melhorar remoÃ§Ã£o de console.logs com Terser e forÃ§ar novo build

--- vite.config.ts
+++ vite.config.ts
@@ -90,7 +90,10 @@ export default defineConfig({
       compress: {
         drop_console: true,
         drop_debugger: true,
-        pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.trace']
+        pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.trace', 'console.warn']
+      },
+      format: {
+        comments: false
       }
     },
     rollupOptions: {
